e53d09b142319c41ec8d716fe0b67b26a2df5ab4
sefatavukcu <tavukcusefa@gmail.com>
2022-11-21 18:14:22 +0300

    21.11.2022

---
+++ Week8 Hoca_Uyg/Proje02_KitabeviApp/ViewModels/KitapListViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Proje02_KitabeviApp.Models;
+
+namespace Proje02_KitabeviApp.ViewModels
+{
+    public class KitapListViewModel
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public int BasimYili { get; set; }
+        public int SayfaSayisi { get; set; }
+        public int KategoriId { get; set; }
+        public int YazarId { get; set; }
+        public Kategori Kategori { get; set; }
+        public Yazar Yazar { get; set; }
+    }
+}
