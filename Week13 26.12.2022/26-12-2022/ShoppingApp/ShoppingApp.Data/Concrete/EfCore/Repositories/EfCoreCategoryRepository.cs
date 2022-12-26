7a15dc479902a9cc8e7d4931504a545a7c94780b
sefatavukcu <tavukcusefa@gmail.com>
2022-12-26 18:11:26 +0300

    26.12.2022

---
+++ Week13 26.12.2022/26-12-2022/ShoppingApp/ShoppingApp.Data/Concrete/EfCore/Repositories/EfCoreCategoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Data.Abstract;
+using ShoppingApp.Data.Concrete.EfCore.Contexts;
+using ShoppingApp.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Concrete.EfCore.Repositories
+{
+    public class EfCoreCategoryRepository : EfCoreGenericRepository<Category>, ICategoryRepository
+    {
+        public EfCoreCategoryRepository(ShopAppContext context) : base(context)
+        {
+            //buraya gelen context base classa gönderiliyor ve aynı zamanda sadece bu constructor içinde geçerli. Ama bu classın bütününde henüz kullanılamıyor. Eğer kullanılsın istersek yapmamız gereken işlemler var.
+        }
+        private ShopAppContext ShopAppContext
+        {
+            get { return _context as ShopAppContext; }
+        }
+        public Category GetByIdWithProducts()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<List<Category>> GetSearchResultAsync(string searchString)
+        {
+            searchString = searchString.ToLower();
+            return await ShopAppContext
+                .Categories
+                .Where(c => c.Name.ToLower().Contains(searchString) || c.Description.ToLower().Contains(searchString)).ToListAsync();
+        }
+    }
+}
---
+++ Week13 26.12.2022/26-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/CategoryListDto.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingApp.Entity.Concrete;
+
+namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class CategoryListDto
+    {
+        public List<Category> Categories { get; set; }
+        public Category Category { get; set; }
+    }
+}
