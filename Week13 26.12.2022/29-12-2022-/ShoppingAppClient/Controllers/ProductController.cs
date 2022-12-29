57eda698b23e32d80401b03814711c9f066081a3
sefatavukcu <tavukcusefa@gmail.com>
2022-12-29 15:02:09 +0300

    29.12.2022

---
+++ Week13 26.12.2022/29-12-2022-/ShoppingAppClient/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShoppingAppClient.Models;
+
+namespace ShoppingAppClient.Controllers
+{
+	public class ProductController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+            var urunler = new List<ProductViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5200/api/products/"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    urunler = JsonConvert.DeserializeObject<List<ProductViewModel>>(apiResponse);
+                }
+            }
+            return View(urunler);
+        }
+        public async Task<IActionResult> Create()
+        {
+            var kategoriler = new List<CategoryViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5200/api/categories/"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    kategoriler = JsonConvert.DeserializeObject<List<CategoryViewModel>>(apiResponse);
+                }
+            }
+            ProductViewModel productViewModel = new ProductViewModel
+            {
+                Categories = kategoriler,
+            };
+            return View(productViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create async adında bir httpResponse metodu kullanacağız.
+            }
+            return View();
+        }
+    }
+}
