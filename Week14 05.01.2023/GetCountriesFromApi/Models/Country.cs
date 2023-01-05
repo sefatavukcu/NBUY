f2767d932838f42db3a68a6833f21f80848325d0
sefatavukcu <tavukcusefa@gmail.com>
2023-01-05 15:02:53 +0300

    05.01.2023

---
+++ Week14 05.01.2023/GetCountriesFromApi/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace GetCountriesFromApi.Models
+{
+    public class Country
+    {
+        public string name { get; set; }
+        public string flag { get; set; }
+        public string capital { get; set; }
+    }
+}
